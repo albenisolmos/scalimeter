d0394abf6853bbdb110a401542715d4bf2e2fc80
albenisolmos <albenisolmos88@gmail.com>
2024-01-06 13:53:29 -0400

    feat: add bindings events

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -2,4 +2,4 @@
     export let value;
 </script>
 
-<input class='box box-border focus:outline-0 w-full h-8 bg-white p-2 focus:border-blue-300 dark:bg-slate-400 focus-within:border-blue-300' bind:value {...$$restProps}/>
+<input class='box box-border focus:outline-0 w-full h-8 bg-white p-2 focus:border-blue-300 focus:border-2 dark:bg-slate-400 focus-within:border-blue-300' bind:value on:change on:keyup on:input {...$$restProps}/>
