7f6cb44d9724d1d2a1be48dc7d277c429435c41e
albenisolmos <albenisolmos88@gmail.com>
2024-01-06 13:54:54 -0400

    fix: mitigate lag on input event

--- src/components/InputUnit.svelte
+++ src/components/InputUnit.svelte
@@ -5,9 +5,18 @@
     export let placeholder = ''
     export let value
 	export let unit
+
+	let timer
+	const waitTime = 200
+	function onKeyUp(event) {
+		clearTimeout(timer)
+		timer = setTimeout(() => {
+			value = event.target.value
+		}, waitTime)
+	}
 </script>
 
 <div class='box wrap w-full h-8 flex flex-shrink items-stretch'>
-	<Input type='number' bind:value {placeholder} min='0' />
+	<Input type='number' inputmode='decimal' on:keyup={onKeyUp}  {placeholder} min='0' value={value} />
 	<DropdownUnits bind:unit />
 </div>
